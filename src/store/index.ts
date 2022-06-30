f6198a9fcac383b017eec44bb8ee270ef1e9c39d
Claudio OrlandI <claudiosorlandi@gmail.com>
2022-06-30 20:27:00 -0300

    :sparkles: feat: configure redux saga middleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,17 @@
-import { legacy_createStore as createStore } from "redux";
+import { applyMiddleware, compose, legacy_createStore as createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import createSagaMiddleware from 'redux-saga';
 
 import reducers from "./reducers";
+import sagas from './sagas/auth';
 
-const store = createStore(reducers, composeWithDevTools());
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(reducers, compose(
+    applyMiddleware(sagaMiddleware),
+    composeWithDevTools(),
+));
+
+sagaMiddleware.run(sagas);
 
 export default store;
